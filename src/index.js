ce500fc536ca985fead034531e68ced10cdaf184
Indrawan Lisanto <indrawan@aleph-labs.com>
2020-04-29 23:29:13 +0700

    completed setup redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { React, ReactDOM, Provider } from 'libraries';
+import { store } from 'modules';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/libraries/index.js
+++ src/libraries/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import PropTypes from "prop-types";
 import moment from "moment";
 import axios from "axios";
 import thunk from "redux-thunk";
 
-export { React, PropTypes, moment, axios, thunk };
+export { React, ReactDOM, PropTypes, moment, axios, thunk };
 
 export * from "react";
 export * from "react-router-dom";
