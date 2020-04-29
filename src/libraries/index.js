5bfdbfff98653d3ff32edb3c62e5379eabdf146c
Indrawan Lisanto <indrawan@aleph-labs.com>
2020-04-29 19:54:17 +0700

    initial commit

---
+++ src/libraries/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
+import axios from "axios";
+
+export { React, PropTypes, moment, axios };
+
+export * from "react";
+export * from "react-router-dom";
+export * from "react-redux";
+export * from "redux-thunk";
